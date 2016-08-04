7b33edb752ce997a169263198ffd6ca01955640d
Michael Williams <michael.williams@enspiral.com>
2016-08-05 01:56:25 +1200

    test before async

--- test/before.js
+++ test/before.js
@@ -13,12 +13,10 @@ function greet (greeting, name) {
   return `${greeting}, ${name}.`
 }
 
-function upper (args) {
-  return args.map(a => a.toUpperCase())
-}
-
-function lower (args) {
-  return args.map(a => a.toLowerCase())
+const upper = argsTo('UpperCase')
+const lower = argsTo('LowerCase')
+function argsTo (name) {
+  return (args) => args.map(a => a['to'+name]())
 }
 
 function preError (args) {
@@ -35,6 +33,50 @@ test('before sync', function (t) {
   t.equal(greet4('Hello', 'Foo'), 'HELLO, FOO.')
   const greet5 = sync(sync(greet, lower), upper)
   t.equal(greet5('Hello', 'Foo'), 'hello, foo.')
+  const greet6 = sync(greet, preError)
+  t.ok(greet6('Hello', 'Foo') instanceof Error)
 
   t.end()
+})
+
+
+function tick (n, cb) {
+  cb(null, n + 1)
+}
+
+function double ([arg], cb) {
+  cb(null, [arg])
+}
+
+function prePass ([arg], cb) {
+  setTimeout(function () {
+    cb(null, [arg * 2])
+  })
+}
+function preFail ([arg], cb) {
+  setTimeout(function () {
+    cb(new Error('fail'))
+  })
+}
+
+test('before async', function (t) {
+  var n = 2
+
+  async(async(tick, prePass), double)
+    (3, function (err, v) {
+      t.error(err)
+      t.equal(v, 7)
+      next()
+    })
+
+  async(async(tick, double), preFail)
+    (3, function (err) {
+      t.ok(err instanceof Error)
+      next()
+    })
+
+  function next () {
+    if(--n) return
+    t.end()
+  }
 })
